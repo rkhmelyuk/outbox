aa9e4ccc114b9554d485d932c371092191967ab4
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2010-08-29 22:26:01 +0300

    #2: Simple members pagination.

---
+++ test/unit/outbox/PaginationTagLibTests.groovy
@@ -0,0 +1,17 @@
+package outbox
+
+import grails.test.*
+
+class PaginationTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
