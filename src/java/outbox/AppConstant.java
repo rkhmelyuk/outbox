9e8b44071e4958b866df6e2672bb835bb090d728
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2010-09-04 02:16:21 +0300

    #3 Application constants

---
+++ src/java/outbox/AppConstant.java
@@ -0,0 +1,13 @@
+package outbox;
+
+/**
+ * @author Ruslan Khmelyuk
+ * @since 2010-09-04
+ */
+public interface AppConstant {
+
+    /**
+     * The salt for generating subscriber id.
+     */
+    String SUBSCRIBER_ID_SALT = "KSielA3!*sCQrPow(+A";
+}
