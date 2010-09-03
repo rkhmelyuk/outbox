5a9d5698f5d0e81540454d3cd0767befc1fca152
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2010-09-04 02:15:59 +0300

    #3 Test subscribers

---
+++ database/patch/0.1/08_Subscriber_dev.sql
@@ -0,0 +1,5 @@
+insert into Subscriber(SubscriberId, FirstName, LastName, Email, Enabled, MemberId, GenderId, LanguageId, TimezoneId, CreateDate) values
+('abcdefghgklmnop0000001', 'Mika', 'Newton', 'ruslan.khmelyuk+outbox@gmail.com', true, 2, null, null, null, '2010-09-03'),
+('abcdefghgklmnop0000001', 'Jacob', 'Fane', 'ruslan.khmelyuk+outbox@gmail.com', true, 2, 1, null, null, '2010-09-03'),
+('abcdefghgklmnop0000001', 'Artur', 'Sir', 'ruslan.khmelyuk+outbox@gmail.com', true, 2, 1, null, null, '2010-09-03'),
+('abcdefghgklmnop0000001', 'May', 'Dacen', 'ruslan.khmelyuk+outbox@gmail.com', false, 2, 2, null, null, '2010-09-03');
