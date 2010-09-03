86ff80faeb279b26c71dee4a4387be690bdaea44
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2010-09-04 02:15:33 +0300

    #3 Move inserting test member to staging patch only.

--- database/patch/0.1/05_Member.sql
+++ database/patch/0.1/05_Member.sql
@@ -38,6 +38,3 @@ create index IX_Member_Email on Member(Email);
 
 insert into Member(FirstName, LastName, Username, Password, Email, TimeZoneId, LanguageId, Enabled, AccountExpired, AccountLocked, PasswordExpired, CreateDate)
 values ('System', 'Administrator', 'sysadmin', 'o73Ss/ScAeOjK/b70TR1r8VF9VrU0nbhGmMNOLgKNPU=', 'sysadmin@localhost', 15, 25, true, false, false, false, '2010-09-01');
-
-insert into Member(FirstName, LastName, Username, Password, Email, TimeZoneId, LanguageId, Enabled, AccountExpired, AccountLocked, PasswordExpired, CreateDate)
-values ('John', 'Smith', 'john', 'a2+HOqaR4USoUn0g41+yxd4OphGDjaI+MygoCvNa8C4=', 'jsmith@localhost', 17, 127, true, false, false, false, '2010-09-01');
---
+++ database/patch/0.1/05_Member_dev.sql
@@ -0,0 +1,2 @@
+insert into Member(FirstName, LastName, Username, Password, Email, TimeZoneId, LanguageId, Enabled, AccountExpired, AccountLocked, PasswordExpired, CreateDate)
+values ('John', 'Smith', 'john', 'a2+HOqaR4USoUn0g41+yxd4OphGDjaI+MygoCvNa8C4=', 'jsmith@localhost', 17, 127, true, false, false, false, '2010-09-01');
