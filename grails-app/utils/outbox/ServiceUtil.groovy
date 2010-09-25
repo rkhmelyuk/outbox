d0077cba80fd241fd52c1c69a2296dbbf6af9620
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2010-09-26 01:12:26 +0300

    ServiceUtil exposes method for rolling back current transaction.

--- grails-app/utils/outbox/ServiceUtil.groovy
+++ grails-app/utils/outbox/ServiceUtil.groovy
@@ -1,10 +1,11 @@
 package outbox
 
+import org.springframework.transaction.NoTransactionException
 import org.springframework.transaction.interceptor.TransactionAspectSupport
 
 /**
  * @author Ruslan Khmelyuk
- * @since  2010-09-19
+ * @since 2010-09-19
  */
 class ServiceUtil {
 
@@ -20,8 +21,22 @@ class ServiceUtil {
 
         boolean saved = (item.save(flush: true) != null)
         if (!saved) {
-            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly()
+            setRollbackTransaction()
         }
         return saved
+    }
+
+    /**
+     * Mark current transaction if any as Rollback Only
+     */
+    static boolean setRollbackTransaction() {
+        try {
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly()
+            return true
+        }
+        catch (NoTransactionException e) {
+            // ignore
+            return false
+        }
     }
 }
