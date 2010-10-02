a0ce843166c70e7240e849b1151a6c04417fc75d
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2010-10-03 00:27:36 +0300

    #22: Insert opening tracking into email in different position in depends on format.

--- src/groovy/outbox/template/builder/TemplateTrackingHeaderFilter.groovy
+++ src/groovy/outbox/template/builder/TemplateTrackingHeaderFilter.groovy
@@ -10,7 +10,25 @@ class TemplateTrackingHeaderFilter implements TemplateFilter {
     String trackingHeader
 
     void filter(TemplateFilterContext context) {
-        context.template = trackingHeader + context.template 
+        def index = indexOf(context.template, '</body>', '</html>')
+        if (index != -1) {
+            def builder = new StringBuilder(context.template)
+            builder.insert(index, trackingHeader)
+            context.template = builder.toString()
+        }
+        else {
+            context.template = trackingHeader + context.template
+        }
+    }
+
+    private int indexOf(String body, String... tags) {
+        for (String tag in tags) {
+            def index = body.indexOf(tag)
+            if (index != -1) {
+                return index
+            }
+        }
+        return -1
     }
 
 }
--- test/unit/outbox/template/builder/TemplateTrackingHeaderFilterTests.groovy
+++ test/unit/outbox/template/builder/TemplateTrackingHeaderFilterTests.groovy
@@ -24,4 +24,34 @@ class TemplateTrackingHeaderFilterTests extends GroovyTestCase {
         assertEquals '[HEADER]test', context.template
     }
 
+    void testFilter_Body() {
+        def context = new TemplateFilterContext(
+                campaign: new Campaign(id: 1),
+                message: new CampaignMessage(id: '2'),
+                subscriber: new Subscriber(id: '3'))
+
+        context.template = '<body>test</body>'
+
+        def filter = new TemplateTrackingHeaderFilter()
+        filter.trackingHeader = '[HEADER]'
+        filter.filter context
+
+        assertEquals '<body>test[HEADER]</body>', context.template
+    }
+
+    void testFilter_Html() {
+        def context = new TemplateFilterContext(
+                campaign: new Campaign(id: 1),
+                message: new CampaignMessage(id: '2'),
+                subscriber: new Subscriber(id: '3'))
+
+        context.template = '<html>test</html>'
+
+        def filter = new TemplateTrackingHeaderFilter()
+        filter.trackingHeader = '[HEADER]'
+        filter.filter context
+
+        assertEquals '<html>test[HEADER]</html>', context.template
+    }
+
 }
