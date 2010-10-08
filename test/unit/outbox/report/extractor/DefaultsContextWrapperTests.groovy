cb8332815872b67e0bac80e03692ece850e15724
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2010-10-09 01:12:17 +0300

    #24: Defaults Context  Extractor Wrapper more tests.

--- test/unit/outbox/report/extractor/DefaultsContextWrapperTests.groovy
+++ test/unit/outbox/report/extractor/DefaultsContextWrapperTests.groovy
@@ -26,4 +26,42 @@ class DefaultsContextWrapperTests extends GrailsUnitTestCase {
 
         extractorControl.verify()
     }
+
+    void testDefaults_AllSet() {
+        def reportResult = new ReportResult()
+
+        def extractorControl = mockFor(ReportExtractor)
+        extractorControl.demand.extract { context ->
+            assertEquals 'set', context.param1
+            assertEquals 'test', context.param2
+            assertEquals '', context.param3
+            return reportResult
+        }
+
+        def wrapper = new DefaultsContextWrapper(extractorControl.createMock(),
+                [param1: 'default1', param2: 'default2', param3: 'default3'])
+
+        assertEquals reportResult, wrapper.extract([param1: 'set', param2: 'test', param3: ''])
+
+        extractorControl.verify()
+    }
+
+    void testDefaults_Null() {
+        def reportResult = new ReportResult()
+
+        def extractorControl = mockFor(ReportExtractor)
+        extractorControl.demand.extract { context ->
+            assertEquals 'default1', context.param1
+            assertEquals 'test', context.param2
+            assertEquals '', context.param3
+            return reportResult
+        }
+
+        def wrapper = new DefaultsContextWrapper(extractorControl.createMock(),
+                [param1: 'default1', param2: 'default2', param3: 'default3'])
+
+        assertEquals reportResult, wrapper.extract([param1: null, param2: 'test', param3: ''])
+
+        extractorControl.verify()
+    }
 }
