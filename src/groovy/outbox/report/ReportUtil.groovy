9e9d2921b95c9c7cd63aa3f47b9e455a06181476
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2010-10-11 01:13:14 +0300

    #24: ReportUtil with tests.

---
+++ src/groovy/outbox/report/ReportUtil.groovy
@@ -0,0 +1,38 @@
+package outbox.report
+
+/**
+ * @author Ruslan Khmelyuk
+ */
+class ReportUtil {
+
+    /**
+     * Gets the best period to show results before two dates,
+     * for example, between campaign start date and today.
+     * <br/>
+     * If end date is null, then current date will be used.
+     *
+     * @param start the start date, can't be null.
+     * @param end the end date.
+     * @return the best fit period
+     */
+    static Period bestPeriod(Date start, Date end = null) {
+        end = end ?: new Date()
+
+        int days = end.minus(start)
+        if (days <= 1) { // up to 1 day
+            return Period.Hour
+        }
+        else if (days <= 3 * 7) { // up to 3 weeks
+            return Period.Day
+        }
+        else if (days < 2 * 30) { // up to 2 month
+            return Period.Week
+        }
+        else if (days < 24 * 30) { // up to 2 years
+            return Period.Month
+        }
+        else {
+            return Period.Year
+        }
+    }
+}
---
+++ test/unit/outbox/report/ReportUtilTests.groovy
@@ -0,0 +1,21 @@
+package outbox.report
+
+/**
+ * @author Ruslan Khmelyuk
+ */
+class ReportUtilTests extends GroovyTestCase {
+
+    void testBestPeriod() {
+        def date = new Date()
+
+        assertEquals Period.Hour, ReportUtil.bestPeriod(date - 1, date)
+        assertEquals Period.Hour, ReportUtil.bestPeriod(date - 1, null)
+        assertEquals Period.Day, ReportUtil.bestPeriod(date - 5, date)
+        assertEquals Period.Day, ReportUtil.bestPeriod(date - 21, date)
+        assertEquals Period.Week, ReportUtil.bestPeriod(date - 50, date)
+        assertEquals Period.Month, ReportUtil.bestPeriod(date - 60, date)
+        assertEquals Period.Month, ReportUtil.bestPeriod(date - 140, date)
+        assertEquals Period.Month, ReportUtil.bestPeriod(date - 440, date)
+        assertEquals Period.Year, ReportUtil.bestPeriod(date - 1440, date)
+    }
+}
