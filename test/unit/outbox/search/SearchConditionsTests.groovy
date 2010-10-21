196892b59c83d82c3374a6001b8f253cedfb0012
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2010-10-21 23:29:55 +0300

    #28: SearchConditions tests

--- test/unit/outbox/search/SearchConditionsTests.groovy
+++ test/unit/outbox/search/SearchConditionsTests.groovy
@@ -15,11 +15,39 @@ class SearchConditionsTests extends GrailsUnitTestCase {
         conditions = new SearchConditions()
     }
 
+    void testFields() {
+        conditions.includeCount = true
+        conditions.includeFound = true
+        conditions.cacheQuery = true
+
+        assertTrue conditions.includeCount
+        assertTrue conditions.includeFound
+        assertTrue conditions.cacheQuery
+    }
+
     void testAddCondition() {
         def condition = new PageCondition()
         conditions.add condition
 
         assertTrue conditions.has(PageCondition)
         assertSame condition, conditions.get(PageCondition)
+    }
+
+    void testGetCondition() {
+        def condition1 = new PageCondition()
+        def condition2 = new OwnedByCondition()
+
+        conditions.add condition1
+        conditions.add condition2
+
+        assertSame condition1, conditions.get(PageCondition)
+        assertSame condition2, conditions.get(OwnedByCondition)
+        assertNull conditions.get(ArchivedCondition)
+    }
+
+    void testHas() {
+        conditions.add new PageCondition()
+        assertTrue conditions.has(PageCondition)
+        assertFalse conditions.has(OwnedByCondition)
     }
 }
