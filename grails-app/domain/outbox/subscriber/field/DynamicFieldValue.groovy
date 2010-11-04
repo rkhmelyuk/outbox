04af4576319f22a2fe6018a6e0b3aa2bef437313
Ruslan Khmelyuk <ruslan.khmelyuk@gmail.com>
2010-11-04 21:51:32 +0200

    #29: Dynamic Field Value + tests.

--- grails-app/domain/outbox/subscriber/field/DynamicFieldValue.groovy
+++ grails-app/domain/outbox/subscriber/field/DynamicFieldValue.groovy
@@ -38,5 +38,80 @@ class DynamicFieldValue {
     }
 
     static constraints = {
+        subscriber nullable: false
+        dynamicField nullable: false
+        stringValue nullable: true, maxSize: 9999
+        numberValue nullable: true
+        booleanValue nullable: true
+        singleItem nullable: true
+    }
+
+    static transients = ['value']
+
+    /**
+     * Gets correct value by dynamic field type.
+     * @return the value by type.
+     */
+    def getValue() {
+        def result = null
+        if (dynamicField?.type != null) {
+            switch (dynamicField.type) {
+                case DynamicFieldType.String:
+                    result = stringValue
+                    break
+                case DynamicFieldType.Number:
+                    result = numberValue
+                    break
+                case DynamicFieldType.Boolean:
+                    result = booleanValue
+                    break
+                case DynamicFieldType.SingleSelect:
+                    result = singleItem
+                    break
+            }
+        }
+        if (result == null) {
+            result = stringValue
+            if (result == null) {
+                result = numberValue
+            }
+            if (result == null) {
+                result = booleanValue
+            }
+            if (result == null) {
+                result = singleItem
+            }
+        }
+        return result
+    }
+
+    /**
+     * Sets correct value by dynamic field type.
+     * @param value the new value.
+     */
+    void setValue(def value) {
+
+        stringValue = null
+        numberValue = null
+        booleanValue = null
+        singleItem = null
+
+        if (dynamicField?.type != null) {
+            switch (dynamicField.type) {
+                case DynamicFieldType.String:
+                    stringValue = value
+                    return
+                case DynamicFieldType.Number:
+                    numberValue = value
+                    return
+                case DynamicFieldType.Boolean:
+                    booleanValue = value
+                    return
+                case DynamicFieldType.SingleSelect:
+                    singleItem = value
+                    return
+            }
+        }
+        stringValue = value
     }
 }
---
+++ test/unit/outbox/subscriber/field/DynamicFieldValueTests.groovy
@@ -0,0 +1,95 @@
+package outbox.subscriber.field
+
+import grails.test.GrailsUnitTestCase
+import outbox.subscriber.Subscriber
+
+/**
+ * @author Ruslan Khmelyuk
+ */
+class DynamicFieldValueTests extends GrailsUnitTestCase {
+
+    void testFields() {
+        def value = new DynamicFieldValue()
+        value.id = 1
+        value.subscriber = new Subscriber(id: '00000')
+        value.dynamicField = new DynamicField(id: 2)
+        value.stringValue = 'test'
+        value.numberValue = 53.25
+        value.booleanValue = true
+        value.singleItem = new DynamicFieldItem(id: 3)
+
+        assertEquals 1, value.id
+        assertEquals 2, value.dynamicField.id
+        assertEquals 3, value.singleItem.id
+        assertEquals 53,25, value.numberValue
+        assertEquals 'test', value.stringValue
+        assertEquals '00000', value.subscriber.id
+        assertTrue value.booleanValue
+    }
+
+    void testGetValue() {
+        def value = new DynamicFieldValue()
+        value.dynamicField = new DynamicField(id: 2)
+        value.stringValue = 'test'
+        value.numberValue = 53.25
+        value.booleanValue = true
+        value.singleItem = new DynamicFieldItem(id: 3)
+
+        assertEquals value.stringValue, value.value
+
+        value.dynamicField.type = DynamicFieldType.String
+        assertEquals value.stringValue, value.value
+
+        value.dynamicField.type = DynamicFieldType.Number
+        assertEquals value.numberValue, value.value
+
+        value.dynamicField.type = DynamicFieldType.Boolean
+        assertEquals value.booleanValue, value.value
+
+        value.dynamicField.type = DynamicFieldType.SingleSelect
+        assertEquals value.singleItem, value.value
+    }
+
+    void testGetValue_OtherType() {
+        def value = new DynamicFieldValue()
+        value.dynamicField = new DynamicField(id: 2)
+        value.stringValue = 'test'
+
+        value.dynamicField.type = DynamicFieldType.String
+        assertEquals value.stringValue, value.value
+
+        value.dynamicField.type = DynamicFieldType.Number
+        assertEquals value.stringValue, value.value
+
+        value.value = 12.34
+        assertEquals value.numberValue, value.value
+        assertNull value.stringValue
+    }
+
+    void testSetValue() {
+        def value = new DynamicFieldValue()
+        value.dynamicField = new DynamicField(id: 2)
+
+        value.value = 'most'
+        assertEquals 'most', value.stringValue
+
+        value.dynamicField.type = DynamicFieldType.String
+        value.value = 'test'
+        assertEquals 'test', value.stringValue
+
+        value.dynamicField.type = DynamicFieldType.Number
+        value.value = 12.22
+        assertEquals 12.22, value.numberValue
+        assertNull value.stringValue
+
+        value.dynamicField.type = DynamicFieldType.Boolean
+        value.value = true
+        assertEquals true, value.booleanValue
+        assertNull value.numberValue
+
+        value.dynamicField.type = DynamicFieldType.SingleSelect
+        value.value = new DynamicFieldItem(id: 2)
+        assertEquals 2, value.singleItem.id
+        assertNull value.booleanValue
+    }
+}
